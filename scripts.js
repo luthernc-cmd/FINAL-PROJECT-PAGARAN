19cc861ceba7661b7b5baa8ce4c5a2118d164683
luther <luthergwapa@gmail.com>
2026-06-18 15:12:11 +0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,24 @@
+```javascript
+function showMessage() {
+    document.getElementById("demo").innerHTML =
+    "Welcome to my Portfolio Website!";
+}
+
+function submitForm() {
+
+    let name = document.getElementById("name").value;
+    let message = document.getElementById("message").value;
+
+    if (name === "" || message === "") {
+
+        document.getElementById("output").innerHTML =
+        "Please complete the form.";
+
+    } else {
+
+        document.getElementById("output").innerHTML =
+        "Thank you, " + name +
+        "! Your message has been submitted successfully.";
+    }
+}
+```
